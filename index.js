ad5ae2a1aae9007a5b33ba20ee9ec742a2751038
WotilaCarneiro <wotila@gmail.com>
2024-07-04 18:46:51 -0300

    Segundo Commit

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
-function ranking (numWins, numLoses){
+ranking(20, 10);
+
+function ranking(numWins, numLoses) {
     let saldo = numWins - numLoses;
     let nivel = "";
-    if (saldo < 10){
-        nivel = "Ferro"
-    }else if (saldo <=11 && saldo <=20){
-        nivel = "Bronze"
-    }
 
+    if (saldo <= 10) {
+        nivel = "Ferro";
+    } else if (saldo >= 11 && saldo <= 20) {
+        nivel = "Bronze";
+    } else if (saldo >= 21 && saldo <= 50) {
+        nivel = "Prata";
+    } else if (saldo >= 51 && saldo <= 80) {
+        nivel = "Ouro";
+    } else if (saldo >= 81 && saldo <= 90) {
+        nivel = "Diamante";
+    } else if (saldo >= 91 && saldo <= 100) {
+        nivel = "Lendário";
+    } else if (saldo > 100) {
+        nivel = "Imortal";
+    } else {
+        nivel = "Nível Desconhecido"
+    }
+    console.log(`O herói tem saldo de ${saldo} vitórias e está no nível ${nivel}`)
 }
